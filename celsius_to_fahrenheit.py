c01064ede8d79dcaf5740864eda76e6f18d285dd
Rahul Sivadasan <39067962+Aizen741@users.noreply.github.com>
2019-06-26 02:02:06 +0530

    Update celsius_to_fahrenheit.py

--- celsius_to_fahrenheit.py
+++ celsius_to_fahrenheit.py
@@ -1,8 +1,8 @@
-celsius = float(input("Write the Temperature in Celsius: "))
+celsius = float(input("Write the Temperature of your body in Celsius: "))
 
 def Fahr(celsius, fahrenheit = 32):
         return celsius*1.8 + fahrenheit
-print("Temperature in fahrenheit is :", Fahr(celsius, fahrenheit = 32))
+print("Temperature in fahrenheit is :", Fahr(celsius, fahrenheit = 32), "F)
 
 if (Fahr(celsius,fahrenheit=32)>100):
     print("Dude! Take medicine, you are sick")
